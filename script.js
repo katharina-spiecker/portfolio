e9492e149697c21bbac3d567138a8000ba2b0235
Katharina Spiecker <katharina.spiecker@solution360.de>
2022-09-18 11:31:52 +0200

    add scrolling effect

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// typewriter effect
 (function(){
     const targetText = document.querySelector(".typewriter");
     const splitTargetText = targetText.innerText.split("");
@@ -20,6 +21,7 @@
     }
 })();
 
+// technology section moving effect
 (function(){
     let transformStyles = ["translateX(15px)", "translateX(-15px)", "translateY(15px)", "translateY(-15px)"];
     let boxes = document.querySelectorAll(".box");
@@ -29,4 +31,30 @@
         let randStyle = transformStyles[Math.floor(Math.random() * transformStyles.length)];
         boxes[rand].style = "transform:" + randStyle;
     })
+})();
+
+// when scrolling over sections, background changes
+(function(){
+    const sections = document.querySelectorAll(".section");
+    const sectionHeight = sections[0].offsetHeight;
+
+    document.addEventListener("scroll", () => {
+        if (window.scrollY < sectionHeight) {
+            let newColor = sections[0].dataset.color;
+            changeColor(newColor);
+        } else if (window.scrollY < sectionHeight * 2) {
+            let newColor = sections[1].dataset.color;
+            changeColor(newColor);
+        } else if (window.scrollY < sectionHeight * 3) {
+            let newColor = sections[2].dataset.color;
+            changeColor(newColor);
+        } else if (window.scrollY < sectionHeight * 4) {
+            let newColor = sections[3].dataset.color;
+            changeColor(newColor);
+        }
+    });
+
+    function changeColor(hexCode) {
+        document.body.style = "background-color:" + hexCode;
+    }
 })();
