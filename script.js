a3eecd9bba9a01276cf7af92194979cb4cc277a1
katharina-spiecker <kathispiecker@gmail.com>
2025-03-31 16:17:31 +0200

    update portfolio

--- script.js
+++ script.js
@@ -1,31 +1,6 @@
-// typewriter effect
-// (function(){
-//     const targetText = document.querySelector(".typewriter");
-//     const splitTargetText = targetText.innerText.split("");
-//     let newTargetText = "";
-//     splitTargetText.forEach((character) => {
-//         newTargetText += "<span>" + character + "</span>";
-//     });
-    
-//     targetText.innerHTML = newTargetText;
-//     let index = 0;
-//     const spans = targetText.querySelectorAll("span");
-//     const timer = setInterval(onTick, 250);
-    
-//     function onTick() {
-//         spans[index].classList.add("visible");
-//         index++;
-//         if (index === spans.length) {
-//             clearInterval(timer);
-//         }
-//     }
-// })();
-
-
 // when scrolling over sections, background changes
 (function(){
     const sections = document.querySelectorAll(".section");
-    let transitionHeight = sections[0].offsetHeight;
     let transformStyles = ["translateX(15px)", "translateX(-15px)", "translateY(15px)", "translateY(-15px)"];
     let boxes = document.querySelectorAll(".box");
 
@@ -41,11 +16,6 @@
         // 5 sections: intro, about, tech stack, projects 1, projects 2
         // some sections need different transition height than others
         // intro
-
-        // if (sections[0].getBoundingClientRect().bottom > 200) {
-        //     let newColor = sections[0].dataset.color;
-        //     changeColor(newColor);
-        // about
         if (sections[0].getBoundingClientRect().bottom > 300) {
             let newColor = sections[0].dataset.color;
             changeColor(newColor);
@@ -67,6 +37,9 @@
         // project 3
         } else if (sections[4].getBoundingClientRect().bottom > 0) {
             let newColor = sections[4].dataset.color;
+            changeColor(newColor);
+        } else if (sections[5].getBoundingClientRect().bottom > 100) {
+            let newColor = sections[5].dataset.color;
             changeColor(newColor);
         }
     });
